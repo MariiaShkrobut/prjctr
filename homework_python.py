561a4860bcade5a87c747cd6909abaeed49ceddd
MaksymBozhko <bmw70300@gmail.com>
2023-02-02 20:59:20 +0200

    minor improvments

--- homework_python.py
+++ homework_python.py
@@ -1,11 +1,9 @@
 #Homework #12
 print('''Hello Artem''')
 result_hm12 = (2**10-16**2)/(8*32)
-# result_text = "The result of homework #12 is " + str(result) 
-print("The result of homework #12 is number " + str(result_hm12))
+print("The result of homework #12 is number " + str(result_hm12).strip('.''0'))
 
 #Homework #13
-
 property_transfer_xml = """
 <con:name>AuthTocken</con:name><con:sourceType>Response</con:sourceType><con:sourceStep>login</con:sourceStep><con:sourcePath>declare namespace ns1='urn:Magento';
 //ns1:loginResponse/loginReturn[1]</con:sourcePath><con:targetType>Request</con:targetType><con:targetStep>multiCall</con:targetStep><con:targetPath>declare namespace urn='urn:Magento';
@@ -13,5 +11,4 @@ property_transfer_xml = """
 """
 result_hm13 = property_transfer_xml.split('con:targetType')
 result_hm13[1] = result_hm13[1].strip('>''/''<')
-
 print('''The result of homework #13 is word "''' + str(result_hm13[1]) + '''"''')
